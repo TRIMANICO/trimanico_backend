e0d2f843b1f89e987e1e06157be53a5921b13c5b
suvadrashrestha <jeenashrestha04@gmail.com>
2024-06-15 09:25:44 +0545

    Added git ignore

--- backend/account/emails.py
+++ backend/account/emails.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from .models import User
 def send_otp_via_email(email):
     subject="your account verification email"
-    otp=random.randint(1000,9999)
+    otp=random.randint(100000,999999)
     message= f' your otp is {otp}'
     email_from=settings.EMAIL_HOST_USER
     send_mail(subject,message,email_from,[email])
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
